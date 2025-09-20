32bf9fb2ddbb4a4e9d0691a2222f8da6047dc14a
mykyt-tech <mykyt-tech@users.noreply.github.com>
2025-09-20 16:25:52 +0200

    feat: add ArrayList for runtime task storage

--- src/List.java
+++ src/List.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+
 public class List {
-    public static void main(String[] args) {}
+    static ArrayList<Task> tasks = new ArrayList<>();
+
+    public static void main(String[] args) {
+    }
 }
