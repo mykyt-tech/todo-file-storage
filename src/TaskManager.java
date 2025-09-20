3363c7b03a891e0fc641162e9bbe97aafd77f0e7
mykyt-tech <mykyt-tech@users.noreply.github.com>
2025-09-20 17:03:46 +0200

    feat: add deleteTask method to TaskManager and encapsulate task list

--- src/TaskManager.java
+++ src/TaskManager.java
@@ -2,13 +2,17 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 public class TaskManager {
-    static ArrayList<Task> tasks = new ArrayList<>();
+    private static final ArrayList<Task> tasks = new ArrayList<>();
 
     public static void main(String[] args) {
     }
 
-    static void addTask(String title, LocalDateTime dueDate) {
+    public static void addTask(String title, LocalDateTime dueDate) {
         int newID = tasks.size() + 1;
         tasks.add(new Task(newID, title, dueDate));
+    }
+
+    public static void deleteTask(int id) {
+        tasks.remove(id);
     }
 }
