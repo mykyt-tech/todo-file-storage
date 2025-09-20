98d5740c954a25bf2f388ce5f169114504fed774
mykyt-tech <mykyt-tech@users.noreply.github.com>
2025-09-20 16:52:32 +0200

    feat: add addTask method to TaskManager

--- src/TaskManager.java
+++ src/TaskManager.java
@@ -1,8 +1,14 @@
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 public class TaskManager {
     static ArrayList<Task> tasks = new ArrayList<>();
 
     public static void main(String[] args) {
+    }
+
+    static void addTask(String title, LocalDateTime dueDate) {
+        int newID = tasks.size() + 1;
+        tasks.add(new Task(newID, title, dueDate));
     }
 }
